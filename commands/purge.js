8561e3e7ed7f21ca1598987f1360b3844810f700
WhatTheHai <haiboy@hotmail.nl>
2023-04-02 19:31:28 +0200

    help

--- commands/purge.js
+++ commands/purge.js
@@ -1,20 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { SnowflakeUtil } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('purge')
         .setDescription('Deletes the specified number of messages from the channel.')
-        .addIntegerOption(option => option.setName('count').setDescription('The number of messages to delete.').setRequired(true))
-        .addIntegerOption(option => option.setName('days').setDescription('The number of days to look back for messages.').setRequired(true)),
+        .addIntegerOption(option => option.setName('count').setDescription('The number of messages to delete.').setRequired(true)),
     async execute(interaction) {
         const count = interaction.options.getInteger('count');
-        const days = interaction.options.getInteger('days');
-        const date = new Date();
-        date.setDate(date.getDate() - days);
-        await interaction.reply(`Deleting ${count} messages older than ${days} days...`);
-        const messages = await interaction.channel.messages.fetch({ before: date });
-        await interaction.channel.bulkDelete(messages.filter(msg => !msg.pinned).first(count), true);
-        return interaction.channel.send(`Deleted ${count} messages older than ${days} days.`).then(msg => {
+        const twoWeeksAgo = SnowflakeUtil.now() - (14 * 24 * 60 * 60 * 1000); // 14 days in milliseconds
+        const messages = await interaction.channel.messages.fetch({ limit: count, before: twoWeeksAgo });
+        await interaction.channel.bulkDelete(messages, true);
+        await interaction.channel.send(`Deleted ${messages.size} messages.`).then(msg => {
             setTimeout(() => msg.delete(), 5000);
         });
     },
