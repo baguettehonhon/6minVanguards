8f803abd6f61d76f1c73343aac92b9c066a82ede
WhatTheHai <haiboy@hotmail.nl>
2023-04-02 19:15:55 +0200

    cleanup

--- commands/clear.js
+++ commands/clear.js
@@ -6,19 +6,8 @@ module.exports = {
 		.setDescription('Clears the channel.'),
 		
 	async execute(interaction) {
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
 		await interaction.reply(`Clearing channel`);
-		//console.log("LOOOOOOOOOOOOOOOOOOOOOOOOOOOGGGG");
-		fetcher =  await interaction.channel.messages.fetch();
-			//.then(message => console.log(message.embeds[0].title))
-			//.catch(console.error);
-		//console.log(fetcher);
+		fetcher = await interaction.channel.messages.fetch();
 		await interaction.channel.bulkDelete(fetcher);
-		return;
-		messages = await interaction.channel;
-		//console.log(messages);
-		//channelFound = await interaction.channel;
-		//channelFound.bulkDelete(messages);
 	},
 };
